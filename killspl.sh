1afeee7dd1a031b8667da96b68d75da34afa1dbf
Gustav_Young <gus.santana@icloud.com>
2022-11-23 13:27:34 -0600

    add killspl
    Fix boot logo loop on stretch systems

---
+++ killspl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Actualizacion por hora de equipos.
+# Version Alpha 0.2 bugfix 015
+#FOR YOUR EYES ONLY CODE PENDING OF APLYING LICENSE PROTECTION
+#Copyright 2014-2016 Gustavo Santana
+#For internal use only
+#Developed for vupen Sync fork for uxmalstream Prototype
+#bugs? contact at gus.santana(at)icloud.com
+sleep 3;
+killall glibimg2;
+killall glibimg2;
+killall glibimg2;
+killall glibimg2;
+exit;
